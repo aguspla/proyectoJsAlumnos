1a6ac43d1075792e2519eb15324331e662e3b201
aguspla <aplaza56@gmail.com>
2023-03-31 12:19:59 -0300

    arregladas algunas cosas

--- js/script.js
+++ js/script.js
@@ -2,39 +2,88 @@
 const inputNombre = document.getElementById('nombre'),
     inputApellido = document.getElementById('apellido'),
     inputMaterias = document.getElementById('nMaterias'),
-    btnAgregarAlumno = document.getElementById('nuevoAlumno');
-
+    btnAgregarAlumno = document.getElementById('nuevoAlumno'),
+    btnTraerAlumnos = document.getElementById('traer'),
+    selecAlumnos = document.getElementById('alumnos');
 
 
 /* Arrays */
 let alumnos = []
-    
+
 /* Funciones */
-function agregarAlumno(){ /* Esta funcion crea un alumno o lo modifica si ya existe, lugo lo envia al local storage */ 
+function agregarAlumno(){ /* Esta funcion crea un alumno o  modifica el numero de materias si ya existe, lugo lo envia al local storage */ 
+    if(inputNombre.value.length >0 && inputApellido.value.length > 0 && parseInt(inputMaterias.value) > 0 ){
     let alumno = new Alumno(inputNombre.value, inputApellido.value, inputMaterias.value);
     if(!(alumnos.find(objeto => objeto.nombre === alumno.nombre && objeto.apellido === alumno.apellido))){
         alumnos.push(alumno);
+        crearLabel()
     }else{
         let alumnoEncontrado = alumnos.findIndex(objeto => objeto.nombre === alumno.nombre && objeto.apellido === alumno.apellido)
         alumnos.splice(alumnoEncontrado, 1, alumno);
     }
     localStorage.setItem('alumnos', JSON.stringify(alumnos));
+    selectorAlumno();
+    btnTraerAlumnos.disabled = true;
+    }else{
+        alert('Las casillas deben tener un dato valido ingresado')
+    }
 }
 
 function traerAlumnos(){ /* trae los alumnos del local storage ingresados en un uso anterior de la aplicacion al array alumnos */
-    alumnos = alumnos.concat(JSON.parse(localStorage.getItem('alumnos')));
+    if(JSON.parse(localStorage.getItem('alumnos')) != null){
+    for(let alumno of JSON.parse(localStorage.getItem('alumnos'))){
+        if(!alumnos.find(objeto => objeto === alumno)){
+    alumnos = alumnos.concat(alumno);
+    console.log(alumno)
+    crearLabel()
+    selectorAlumno()
+    }
+    }
+    }
+    btnTraerAlumnos.disabled = true;
 }
 
-function selectorAlumno(){ /* la consola me tira error script.js:33 Uncaught TypeError: Cannot read properties of null (reading 'nombre')
-at selectorAlumno (script.js:33:27)
-at script.js:74:1 no se por que, ya que si alumnos esta vacio no deberia entrar al if */
+function selectorAlumno(){ /* Crea una lista para elegir los alumnos */
     if(alumnos.length > 0){
-        let divAlumno = document.createElement('div');
-        document.body.appendChild(divAlumno)
-        for(let i = 0; i< alumnos.length; i ++){
-            divAlumno.innerHTML+= `<label for="alumnos">Elige un Alumno:</label><select id="alumnos"></select><br><option value="${alumnos[i].nombre}${alumnos[i].apellido}">${alumnos[i].nombre}${alumnos[i].apellido}</option>`
+        if(!document.getElementById('alumnos')){
+        let selecAlumno = document.createElement('select')
+        selecAlumno.setAttribute('id','alumnos')
+        document.body.appendChild(selecAlumno);
         }
+        let optAlumno
+        let opcAlumno
+        for(let i = 0; i< alumnos.length; i ++){
+            if(!document.getElementById(`${alumnos[i].nombre}${alumnos[i].apellido}`)){
+            optAlumno = document.createElement('option');
+            optAlumno.setAttribute('value',`${alumnos[i].nombre}${alumnos[i].apellido}`)
+            optAlumno.setAttribute('id',`${alumnos[i].nombre}${alumnos[i].apellido}`)
+            opcAlumno = document.createTextNode(`${alumnos[i].nombre} ${alumnos[i].apellido}`)
+            optAlumno.appendChild(opcAlumno);
+            document.getElementById('alumnos').appendChild(optAlumno)
+            }
+        }
+        
+        
     }
+}
+
+function creadoAlumnos(){
+    if(getElementById('alumnos')){
+        console.log(document.getElementById('alumnos').value)
+    }
+}
+
+function crearLabel(){/* Crea un label */
+    if(!document.getElementsByClassName('alumnos').length){
+        let labAlumno = document.createElement('label');
+        let labAlumnoText = document.createTextNode('Seleccione un Alumno:');
+        labAlumno.setAttribute('class', 'alumnos');
+        labAlumno.setAttribute('for', 'alumnos');
+        labAlumno.appendChild(labAlumnoText);
+        let separacion = document.createElement('br')
+        document.body.appendChild(labAlumno)
+        document.body.appendChild(separacion)
+    }    
 }
 
 
@@ -67,8 +116,7 @@ class Alumno{
 /* Listeners */
 
 btnAgregarAlumno.addEventListener('click', agregarAlumno);
-
+btnTraerAlumnos.addEventListener('click', traerAlumnos);
+selecAlumnos.addEventListener('change', creadoAlumnos);
 
 /* Programa */
-traerAlumnos()
-selectorAlumno()
